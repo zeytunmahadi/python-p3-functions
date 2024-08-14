51a83c6f41cd4a36e6def02f2f6bfb4b1901decd
zeitun <zeytunmahadi718@gmail.com>
2024-08-14 12:33:53 +0300

    Identify key differences between functions in JavaScript and Python

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,45 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print (f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet_with_default("Alice")  
+
+
+greet_with_default()       
+
+   
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+
+result1 = add(5, 3)
+print(result1) 
+
+result2 = add(-1, 4)
+print(result2)
+
+
+
 
 def halve(number):
-    pass
+    return number / 2
+
+
+result1 = halve(10)
+print(result1) 
+
+result2 = halve(7)
+print(result2) 
+
